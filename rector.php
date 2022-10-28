e48c30667059ae85213104bdaf559e673cd08d53
Dmitrii Derepko <xepozz@list.ru>
2022-10-29 00:23:30 +0300

    Fix rector config

--- rector.php
+++ rector.php
@@ -9,7 +9,6 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
     ]);
 
     // register a single rule
