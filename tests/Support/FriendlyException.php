531edb32bd094a6c16032267bb69421faa6019a9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-12-05 11:26:58 +0300

    Add tests

---
+++ tests/Support/FriendlyException.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\FriendlyException\Tests\Support;
+
+use Exception;
+use Yiisoft\FriendlyException\FriendlyExceptionInterface;
+
+final class FriendlyException extends Exception implements FriendlyExceptionInterface
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     */
+    private $solution;
+
+    public function __construct(string $name = 'test', ?string $solution = null)
+    {
+        $this->name = $name;
+        $this->solution = $solution;
+        parent::__construct();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getSolution(): ?string
+    {
+        return $this->solution;
+    }
+}
