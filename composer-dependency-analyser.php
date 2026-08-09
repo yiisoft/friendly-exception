786ff8e0da7d17e4644ccde63d6b21afd0169431
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-09 15:18:09 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -7,5 +7,5 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 return (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
-    ->addPathToScan(__DIR__ . '/src', isDev: false)
-    ->addPathToScan(__DIR__ . '/tests', isDev: true);
+    ->addPathToScan(__DIR__ . '/src', false)
+    ->addPathToScan(__DIR__ . '/tests', true);
